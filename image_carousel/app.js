6dc5427a8bd2da8960279a5c8eb78822c9810f50
Brady <brady.savarie@hotmail.com>
2023-03-31 19:52:27 -0400

    Make next button functional

--- image_carousel/app.js
+++ image_carousel/app.js
@@ -10,3 +10,16 @@ const slideWidth = slides[0].getBoundingClientRect().width;
 slides.forEach((slide, index) => {
     slide.style.left = `${slideWidth * index}px`;
 });
+
+// When I click right, move 1 slide to the right
+
+nextBtn.addEventListener('click', () => {
+    // move slide
+    const currentSlide = track.querySelector('.current__slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const amountToMove = nextSlide.style.left;
+
+    track.style.transform = `translateX(-${amountToMove})`;
+    currentSlide.classList.remove('current__slide');
+    nextSlide.classList.add('current__slide');
+});
